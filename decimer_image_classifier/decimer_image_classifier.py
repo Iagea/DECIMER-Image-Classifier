cc437015797e0ab6cf9b0d83caa5b723d3213f98
Otto Brinkhaus <brinkhaus@gmail.com>
2022-08-22 21:05:14 +0200

    #3

--- decimer_image_classifier/decimer_image_classifier.py
+++ decimer_image_classifier/decimer_image_classifier.py
@@ -1,4 +1,5 @@
 import os
+from copy import copy
 from PIL import Image
 import tensorflow as tf
 import tensorflow.keras as keras
@@ -17,7 +18,10 @@ class DecimerImageClassifier:
             tf.config.experimental.set_memory_growth(gpu, True)
 
     def is_chemical_structure(
-        self, img: Image = False, img_path: str = False, threshold: float = 0.000089
+        self,
+        img: Image = False,
+        img_path: str = False,
+        threshold: float = 0.000089
     ) -> bool:
         """
         This function determines whether or not a given image (given as
@@ -39,7 +43,11 @@ class DecimerImageClassifier:
         else:
             return False
 
-    def get_classifier_score(self, img: Image = False, img_path: str = False) -> float:
+    def get_classifier_score(
+        self,
+        img: Image = False,
+        img_path: str = False
+    ) -> float:
         """
         Function to compute the classifier score for a particular image.
 
@@ -53,7 +61,7 @@ class DecimerImageClassifier:
         """
         if img_path:
             img = Image.open(img_path)
-        img = self.get_resized_grayscale_image(img)
+        img = self._get_resized_grayscale_image(img)
         img_array = keras.preprocessing.image.img_to_array(img)
         img_array = tf.expand_dims(img_array, 0)
         img_array = keras.applications.efficientnet.preprocess_input(img_array)
@@ -61,7 +69,11 @@ class DecimerImageClassifier:
         score = tf.nn.sigmoid(predictions[0])
         return score.numpy()[0]
 
-    def get_resized_grayscale_image(self, img: Image, desired_size: int = 224) -> Image:
+    def _get_resized_grayscale_image(
+        self,
+        img: Image,
+        desired_size: int = 224
+    ) -> Image:
         """
         This function takes a PIL.Image object, converts it to grayscale and
         resizes it to a square image of length/height a given desired_size.
@@ -75,15 +87,61 @@ class DecimerImageClassifier:
         Returns:
             Image: Resized grayscale image
         """
-        grayscale_image = img.convert("L")
+        grayscale_image = self._get_RGB_grayscale_image(img)
+        resized_grayscale_image = self._get_resized_image(grayscale_image,
+                                                          desired_size)
+        return resized_grayscale_image
+
+    def _get_resized_image(
+        self,
+        img: Image,
+        desired_size: int = 224
+    ) -> Image:
+        """
+        This function takes a PIL.Image object, resizes it to a
+        square image of length/height a given desired_size.
+        It returns the resized grayscale image.
+
+        Args:
+            img (Image): PIL.Image object (mode: "L" (grayscale))
+            desired_size (int, optional): Desired image height/length.
+                                          Defaults to 224.
+
+        Returns:
+            Image: Resized grayscale image
+        """
         old_size = img.size
-        if old_size[0] or old_size[1] != desired_size:
-            ratio = float(desired_size) / max(old_size)
-            new_size = tuple([int(x * ratio) for x in old_size])
-            grayscale_image = grayscale_image.resize(new_size, Image.LANCZOS)
-        resized_image = Image.new("L", (desired_size, desired_size), "white")
-        resized_image.paste(
-            grayscale_image,
-            ((desired_size - new_size[0]) // 2, (desired_size - new_size[1]) // 2),
-        )
+        if(old_size[0] or old_size[1] != desired_size):
+            ratio = float(desired_size)/max(old_size)
+            new_size = tuple([int(x*ratio) for x in old_size])
+            img = img.resize(new_size, Image.LANCZOS)
+        resized_image = Image.new('RGB', (desired_size, desired_size), 'white')
+        resized_image.paste(img,
+                            ((desired_size-new_size[0])//2,
+                             (desired_size-new_size[1])//2))
         return resized_image
+
+    def _get_RGB_grayscale_image(
+        self,
+        img: Image
+    ) -> Image:
+        """
+        This function takes a PIL.Image object, and returns the
+        same Image object as a grayscale RGB image (no colours,
+        but three channels).
+
+
+        Args:
+            img (Image): PIL.Image object (mode: "RGB")
+
+        Returns:
+            Image: Grayscale image
+        """
+        if img.mode == 'RGBA':
+            orig_img = copy(img)
+            orig_img.load()
+            img = Image.new('RGB', orig_img.size, (255, 255, 255))
+            img.paste(orig_img, mask=orig_img.split()[3])
+            img.save('caffeine_mod.png')
+        img = img.convert('L').convert('RGB')
+        return img
