3e2b98c849ab5d4844d885806ca8074235d03c52
Kohulan Rajan <kohulan.rajan@uni-jena.de>
2023-07-17 11:09:23 +0200

    fix: version bump on Python

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="decimer_image_classifier",
-    version="1.0.0",
+    version="1.0.1",
     author="Isabel Agea",
     author_email="Maria.Isabel.Agea.Lorente@vscht.cz",
     maintainer="Isabel Agea",
@@ -18,17 +18,17 @@ setuptools.setup(
     url="https://github.com/Iagea/DECIMER-Image-Classifier",
     packages=setuptools.find_packages(),
     license="MIT",
-    install_requires=["tensorflow==2.7.0", "ipyplot", "pillow>=8.2.0"],
+    install_requires=["tensorflow==2.12.0", "ipyplot", "pillow>=8.2.0"],
     package_data={"decimer_image_classifier": ["*.*", "model/*.*", "model/*/*.*"]},
     classifiers=[
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3 :: Only",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.5",
+    python_requires=">=3.8",
 )
